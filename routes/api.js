cc8c76b18b8d2966f087d111f96142f0f831d951
Bruno Freijomil <bruno_freijomil@hotmail.com>
2022-11-28 18:27:58 -0300

    back todo funcional

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer');
 
+// subir y modificar imagenes al Cloudinary y reflejarlas en web
 router.get('/novedades', async function (req, res, next) {
     let novedades = await novedadesModel.getNovedades();
 
@@ -26,6 +28,32 @@ router.get('/novedades', async function (req, res, next) {
     });
     res.json(novedades);
 });
+
+// recibir info del formulario al mail
+router.post('/contacto', async (req, res) => {
+    const mail = {
+        to: 'bruno_freijomil@hotmail.com',
+        subject: 'Contacto web Escuela de Manejo',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere más información a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} <br> Su tel es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await transport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: 'Mensaje enviado'
+    });
+});
+
 
 
 module.exports = router;
